2c06d35ee549c878f60d5aa3ae56db031fc55f33
namphd00769 <namphd00769@fpt.edu.vn>
2019-02-13 23:31:52 +0700

    person contructor

--- js/handling.js
+++ js/handling.js
@@ -1,3 +1,9 @@
-function handling() {
-   alert("ok");
+function Person(first, last, age, eye) {
+   this.firstName = first;
+   this.lastName = last;
+   this.age = age;
+   this.eyeColor = eye;
 }
+
+var myFather = new Person("John", "Doe", 50, "blue");
+alert('"My father is " + myFather.firstName')
